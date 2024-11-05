a6583572f38594d7ecba756b2530433a75d45496
Virtual Jasper <jasper2virtual@hotmail.com>
2024-11-05 15:42:34 +0800

    update base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/fem-repo-four-card-feature-section-figma/',
+  base: '/fem-repo-four-card-feature-section/',
   plugins: [vue()],
 })
